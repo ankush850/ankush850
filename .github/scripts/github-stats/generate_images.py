42a55016b3887488bfd6ce38e91b4c7b9a074f0f
ankush850 <ankushsinghrawat154@gmail.com>
2026-07-15 11:16:36 +0530

    fix: fallback to GITHUB_TOKEN if ACCESS_TOKEN is missing

--- .github/scripts/github-stats/generate_images.py
+++ .github/scripts/github-stats/generate_images.py
@@ -102,8 +102,9 @@ async def main() -> None:
     """
     access_token = os.getenv("ACCESS_TOKEN")
     if not access_token:
-        # access_token = os.getenv("GITHUB_TOKEN")
-        raise Exception("A personal access token is required to proceed!")
+        access_token = os.getenv("GITHUB_TOKEN")
+        if not access_token:
+            raise Exception("A personal access token is required to proceed!")
     user = os.getenv("GITHUB_ACTOR")
     exclude_repos = os.getenv("EXCLUDED")
     exclude_repos = ({x.strip() for x in exclude_repos.split(",")}
